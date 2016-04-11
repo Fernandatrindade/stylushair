7434b1c2dac4998764811d8e64771fd993351ee5
Fernanda Rodrigues <fernandatrinyd@hotmail.com>
2016-04-11 19:28:02 -0300

    Trabalho de conclucao

---
+++ src/main/java/com/fpu/stylushair/StylushairApplication.java
@@ -0,0 +1,12 @@
+package com.fpu.stylushair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylushairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylushairApplication.class, args);
+	}
+}
