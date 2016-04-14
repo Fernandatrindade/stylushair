df79fc3ae8cbcb05ab5de1b4374d3534a6c8786a
Fernanda Rodrigues <fernandatrinyd@hotmail.com>
2016-04-14 01:09:39 -0300

    Commit 1.2

--- src/main/java/com/fpu/stylushair/controller/TituloController.java
+++ src/main/java/com/fpu/stylushair/controller/TituloController.java
@@ -1,14 +1,28 @@
 package com.fpu.stylushair.controller;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import com.fpu.stylushair.model.Usuario;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroTitulo";
 		
 	}
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Usuario usuario){
+	//TODo: Salvar no banco de dados
+		 
+		 System.out.println(">>>>" +usuario.getNome());
+		 return "CadastroTitulo";
+}
 }
